8db12515966340b7b0f9ae61910590beceed8cb1
minhye9731 <jenney62639731@gmail.com>
2023-09-28 12:05:57 +0900

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by 강민혜 on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
