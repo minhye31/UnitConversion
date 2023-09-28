497483eb042f2db78f2f896fc1f7857cba3469a9
minhye9731 <jenney62639731@gmail.com>
2023-09-28 12:56:10 +0900

    feat : setting variables

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -8,6 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum LengthUnit {
+        case Meters
+        case KiloMeters
+        case Feet
+        case Yard
+        case Miles
+        
+        var title: String {
+            switch self {
+            case .Meters: return "meters"
+            case .KiloMeters: return "kilometers"
+            case .Feet: return "feet"
+            case .Yard: return "yard"
+            case .Miles: return "miles"
+            }
+        }
+        
+        
+        
+    }
+    
+    let lengthUnits: [LengthUnit] = [.Meters, .KiloMeters, .Feet, .Yard, .Miles]
+    
+    @State private var selectedInputUnit = LengthUnit.Meters
+    @State private var selectedOutputUnit = LengthUnit.Meters
+    @State private var inputAmount = 0.0
+    
+    @FocusState private var amountIsFocused: Bool
+    
+
+    var convertedResult: Double {
+        
+        // conversion to meters
+        
+        
+        // conversion to selected output unit
+        
+        
+        
+        return 0.0
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -15,7 +58,6 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
-        .padding()
     }
 }
 
